fb93d4da83a4786ccf78b6ced39103f328c67b35
changediff <changediff@outlook.com>
2023-01-05 22:02:15 +0800

    fix

--- migrate2rocky/update.sh
+++ migrate2rocky/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## pre migrate
 yum clean all
 yum update -y
 
@@ -7,5 +8,18 @@ mv /etc/yum.repos.d /etc/yum.repos.d.bak
 mkdir /etc/yum.repos.d/
 cp ./CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
 
+## do migrate
 chmod +x ./migrate2rocky.sh
 ./migrate2rocky.sh -r
+
+## use ustc mirror
+sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+    -e 's|^#baseurl=http://dl.rockylinux.org/$contentdir|baseurl=https://mirrors.ustc.edu.cn/rocky|g' \
+    -i.bak \
+    /etc/yum.repos.d/Rocky-AppStream.repo \
+    /etc/yum.repos.d/Rocky-BaseOS.repo \
+    /etc/yum.repos.d/Rocky-Extras.repo \
+    /etc/yum.repos.d/Rocky-PowerTools.repo
+
+## do update
+yum update -y
