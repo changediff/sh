69c9c6531f09705e1692eefe95e69b3939393205
changediff <changediff@outlook.com>
2023-01-08 12:33:48 +0800

    add homebrew

---
+++ macos/homebrew_init.sh
@@ -0,0 +1,22 @@
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew install apps
+brew install \
+htop \
+md5sha1sum \
+neofetch \
+pyenv
+
+brew install --cask \
+visual-studio-code \
+microsoft-remote-desktop \
+microsoft-edge \
+firefox \
+karabiner-elements \
+clipy \
+multipass \
+workflowy \
+bartender \
+balenaetcher
